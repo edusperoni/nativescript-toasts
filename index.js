18b6c689e92c27cfb6946dd51317a7ae05a1e98d
Dick Smith <richard.smith@ugroupmedia.com>
2017-07-11 14:13:11 -0400

    Made ES5 friendly for UglifyJS Updated Pod and usage to 3.1

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
 var app = require("application");
 var frameModule = require("ui/frame");
 
-
 /**
  * The time duration for which toast will be displayed on the screen
  * For Android the values will be mapped to Toast.LENGTH_LONG and Toast.LENGTH_SHORT
  * For iOS the values will be mapped to the numeric values of Android enum which are 3.5 for LONG and 2 for SHORT
  */
-const DURATION = {
+var DURATION = {
   LONG: "long",
   SHORT: "short"
-}
+};
 exports.DURATION = DURATION;
 
-const POSITION = {
+var POSITION = {
   TOP: 'top',
   BOTTOM: 'bottom',
   CENTER: 'center'
-}
+};
 exports.POSITION = POSITION;
 
 /**
@@ -27,58 +26,41 @@ exports.POSITION = POSITION;
  * @return {void}
  */
 exports.show = function (toastObject) {
-  var duration;
+  var position;
   if (app.ios) {
-    duration = 2.0;
-    if (toastObject.duration === DURATION.LONG) {
-      duration = 3.5;
+    switch (toastObject.position) {
+      case POSITION.TOP:
+        position = "CSToastPositionTop";
+        break;
+      case POSITION.CENTER:
+        position = "CSToastPositionCenter";
+        break;
+      default:
+        position = "CSToastPositionBottom"; //default position = bottom
     }
-    CSToastManager.setDefaultDuration(duration);
-
-    //set position if it is in the object
-    let position = "CSToastPositionBottom"; //default position = bottom
-    
-    if (toastObject.position) {
-      switch (toastObject.position) {
-        case POSITION.TOP:
-          position = "CSToastPositionTOP";
-          break;
-        case POSITION.CENTER:
-          position = "CSToastPositionCenter";
-          break;
-      }
-    }
-
-    CSToastManager.setDefaultPosition(position);
-
-    frameModule.topmost().ios.controller.view.makeToast(toastObject.text);
+    frameModule.topmost().ios.controller.view.makeToastDurationPosition(
+      toastObject.text,
+      toastObject.duration === DURATION.LONG ? 3.5 : 2.0,
+      position
+    );
 
   } else if (app.android) {
-
-    const Toast = android.widget.Toast;
-    duration = Toast.LENGTH_SHORT;
-    if (toastObject.duration === DURATION.LONG) {
-      duration = Toast.LENGTH_LONG;
+    switch (toastObject.position) {
+      case POSITION.TOP:
+        position = android.view.Gravity.TOP;
+        break;
+      case POSITION.CENTER:
+        position = android.view.Gravity.CENTER;
+        break;
+      default:
+        position = android.view.Gravity.BOTTOM; //default position = bottom
     }
-
-    const Gravity = android.view.Gravity;
-
-    //set position if it is in the object
-    let position = Gravity.BOTTOM; //default position = bottom
-    
-    if (toastObject.position) {
-      switch (toastObject.position) {
-        case POSITION.TOP:
-          position = Gravity.TOP;
-          break;
-        case POSITION.CENTER:
-          position = Gravity.CENTER;
-          break;
-      }
-    }
-    console.log(position);
-    var toast = Toast.makeText(app.android.context, toastObject.text, duration);
+    var toast = android.widget.Toast.makeText(
+      app.android.context,
+      toastObject.text,
+      toastObject.duration === DURATION.LONG ? android.widget.Toast.LENGTH_LONG : android.widget.Toast.LENGTH_SHORT
+    );
     toast.setGravity(position, 0, 0);
     toast.show();
   }
-}
+};
